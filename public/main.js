f9a10e31365b5b038744ba946348f575f53d4782
Waseem Maya <waseemmayaa@gmail.com>
2018-08-21 23:50:12 +0500

    pushin new codes

--- public/main.js
+++ public/main.js
@@ -1,12 +1,11 @@
-// Get a reference to the database service
 
 function addTask() {
-  var database = firebase.database();
-  let title = document.getElementById("title").value;
-  let body = document.getElementById("body").value;
-  let taskID = Math.floor(100444000 + Math.random() * 90012000);
+  var database = firebase.database()
+  var title = document.getElementById("title").value;
+  var body = document.getElementById("body").value;
+  var taskID = Math.floor(100444000 + Math.random() * 90012000);
 
-  let allTasks = database.ref(`All Tasks/${taskID}`);
+  var allTasks = database.ref(`All Tasks/${taskID}`);
   allTasks.set({
     Title: title,
     Description: body,
@@ -20,10 +19,49 @@ function addTask() {
 }
 
 function fetchTask() {
-  let taskDiv = document.getElementById("task");
-  var database = firebase.database();
-  let tasksRef = database.ref(`All Tasks`);
-  tasksRef.on("child_added", task => {
-    console.log(task.val());
+  var taskDiv = document.getElementById("tasks");
+  var database = firebase.database()
+  var tasksRef = database.ref(`All Tasks`);
+  tasksRef.on('child_added', function(snapshot) {
+    var getTask = gettingData(snapshot.val(), snapshot.key);
+    taskDiv.innerHTML += getTask;
   });
+
+  function gettingData(snapshot, key) {
+    const rr = `
+    <li class="list-group mb-1">
+        <ul class="list-group-item">Title : ${
+          snapshot.Title
+        }</ul>
+        <ul class="list-group-item">Body : ${snapshot.Description}</ul>
+        <ul class="list-group-item">Time : ${snapshot.date}</ul>
+         <ul class="list-group-item"><button onClick="deleteTask(${snapshot.taskID})" class="myButton btn btn-sm btn-danger float-right">
+                               Delete
+                               </button>
+                               <button onClick="editTask(${
+                                 snapshot.taskID
+                               },'${
+snapshot.Title
+}')" class="myButton btn btn-sm btn-warning float-right">
+                               Edit 
+                               </button></ul>
+       
+    </li>
+    `;
+    return rr;
 }
+}
+
+function deleteTask(taskID) {
+  var database = firebase.database()
+  var deleteTask = database.ref(`All Tasks/${taskID}`);
+  deleteTask.remove().then(function () {
+    
+    console.log("deleting", taskID);
+    console.log("fetching newest data");
+    
+
+  })
+ 
+}
+
